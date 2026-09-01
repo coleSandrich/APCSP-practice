3610ddf4bec7d82e78d2472d7832cf0b50012279
coleSandrich <Cole.sandrich27@student.tamdistrict.org>
2026-09-01 18:34:04 +0000

    i wish i knew how to code

---
+++ topic-3-4/Sequences_practice.py
@@ -0,0 +1,15 @@
+scores = [72, 86, 91, 68, 88]
+title = "Weekly score report" 
+print(scores[0])
+print(scores[2])
+print(scores[4])
+print(scores)
+scores.append(93)
+weekly = title[0:6]
+report = title[13:]
+print(weekly)
+print(report)
+label = weekly + ": " + str(scores[2])
+print(label)
+print(scores)
+#multiple values vs. one value
