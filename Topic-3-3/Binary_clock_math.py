15af6ebb4ebff9fb7814f1728a1c70c257b4810c
coleSandrich <Cole.sandrich27@student.tamdistrict.org>
2026-09-03 18:43:11 +0000

    I tried to add a bit list and make the text say 21. it doesn't work yet :(

--- Topic-3-3/Binary_clock_math.py
+++ Topic-3-3/Binary_clock_math.py
@@ -31,5 +31,21 @@ selected_index = 2
 clock_value = clock_values[selected_index]
 label = labels[selected_index]
 
-bits = [0, 0, 0, 0, 0,]
-bit.list
+bit_list = [64, 32, 16, 8, 4, 2, 1]
+
+remaining = clock_value
+
+bit_list[5] = remaining % 2
+remaining = remaining // 2
+bit_list[4] = remaining % 2
+remaining = remaining // 2
+bit_list[3] = remaining % 2
+remaining = remaining // 2
+bit_list[2]= remaining % 2
+remaining = remaining // 2
+bit_list[1] = remaining % 2
+remaining = remaining // 2
+bit_list[0] = remaining % 2
+
+print(bit[0:])
+bit_text = str(bit_list[1]) + str(bit_list[2]) + str(bit_list[3]) + str(bit_list[4]) + str(bit_list[5])
